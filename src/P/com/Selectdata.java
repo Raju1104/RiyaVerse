6c27932f2d5676ebd0a5e034c27a3e7598bb68b7
Raju1104 <137985960+Raju1104@users.noreply.github.com>
2023-06-29 15:39:24 +0530

    Update Selectdata.java

--- src/P/com/Selectdata.java
+++ src/P/com/Selectdata.java
@@ -1,6 +1,7 @@
 package P.com;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -12,49 +13,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Selectdata extends HttpServlet {
+import com.mysql.cj.protocol.Resultset;
+
+public class Singledata extends HttpServlet {
 
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-		int id = Integer.parseInt(request.getParameter("id"));
-		
+		int id = Integer.parseInt(req.getParameter("id"));
 
 		try {
-
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedata", "root",
-					"root");
-			PreparedStatement ps = con.prepareStatement("select * from empdata where id = ?");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedata", "root", "root");
+
+			PreparedStatement ps = con.prepareStatement("Select * from empdata where id = ?");
 
 			ps.setInt(1, id);
 
 			ResultSet rs = ps.executeQuery();
+
 			rs.next();
+			
+			req.setAttribute("id", rs.getInt(1));
+			req.setAttribute("Firstname", rs.getString(2));
+			req.setAttribute("Lastname", rs.getString(3));
+			req.setAttribute("Email", rs.getString(4));
+			req.setAttribute("Address", rs.getString(5));
+			req.setAttribute("Pincode", rs.getString(6));
 
-			String fname = rs.getString("First Name");
-			String lname = rs.getString("Last Name");
-			String email = rs.getString("Email");
-			String ap = rs.getString("Address");
-			String pin = rs.getString("Pincode");
-
-			EmployeeData employee = new EmployeeData(fname, lname, email, ap, pin);
-		request.setAttribute("employee", employee);
+			RequestDispatcher rd = req.getRequestDispatcher("get.jsp");
+			rd.forward(req, res);
 
 			System.out.println(rs);
-			System.out.println(rs.getString("id"));
+
 			con.close();
+
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
-
-		
-
-		RequestDispatcher rd = request.getRequestDispatcher("Singledata.jsp");
-		rd.forward(request, response);
-
 	}
 
 }
